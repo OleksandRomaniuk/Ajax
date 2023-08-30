b114d7df71c11c3b51454a1164958156ff4c86e4
Олександр Романюк <oleksandrromanuk@Noutbuk-Oleksandr.local>
2023-08-30 14:52:07 +0300

    Create project

---
+++ src/main/kotlin/com/example/ajaxproject/AjaxProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ajaxproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AjaxProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AjaxProjectApplication>(*args)
+}
