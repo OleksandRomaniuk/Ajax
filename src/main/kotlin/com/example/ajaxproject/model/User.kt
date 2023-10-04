b6cbaf7466aee74abeb6b206a684fd8a677b5330
oleksandrromanuk <alehandr.romaniuk@gmail.com>
2023-10-04 11:02:05 +0300

    Merge all

--- src/main/kotlin/com/example/ajaxproject/model/User.kt
+++ src/main/kotlin/com/example/ajaxproject/model/User.kt
@@ -1,14 +1,12 @@
 package com.example.ajaxproject.model
 
 import jakarta.persistence.Id
-import jakarta.persistence.Table
+import org.springframework.data.mongodb.core.mapping.Document
 
-@Table(name = "users")
+@Document("users")
 data class User(
-
     @Id
     val id: String,
-
     val email: String = "",
     val password: String = "",
 )
