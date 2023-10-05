1e021ef9faf588ab41ce7672e69ad80acabe7cd8
oleksandrromanuk <alehandr.romaniuk@gmail.com>
2023-10-05 11:42:27 +0300

    See if in test problems

--- web/src/test/kotlin/com/example/ajaxproject/NatsControllersTest.kt
+++ web/src/test/kotlin/com/example/ajaxproject/NatsControllersTest.kt
@@ -45,63 +45,6 @@ class NatsControllersTest {
         setPassword("password")
     }.build()
 
-    @Test
-    fun `Positive create user`() {
-
-        userRepository.save(userToSave)
-
-        val request = CreateUserRequest.newBuilder().setUser(protoUser).build()
-
-        val expectedResponse = CreateUserResponse.newBuilder().apply {
-            successBuilder.setUser(protoUser)
-        }.build()
-
-        val actual = doRequest(NatsSubject.ADD_USER, request, CreateUserResponse.parser())
-
-        assertThat(actual).isEqualTo(expectedResponse)
-    }
-
-    @Test
-    fun `Success response for get all users`() {
-
-        val request = GetAllUsersRequest.newBuilder().build()
-
-        val protoUserList = userRepository.findAll().map { it.toProtoUser() }
-
-        val expectedResponse = GetAllUsersResponse.newBuilder().apply {
-            successBuilder.setUsers(UserList.newBuilder().addAllUser(protoUserList))
-        }.build()
-
-        val actual = doRequest(NatsSubject.GET_ALL_USERS, request, GetAllUsersResponse.parser())
-
-        assertThat(actual).isEqualTo(expectedResponse)
-    }
-
-    @Test
-    fun `Success response for delete user`() {
-
-        val request = DeleteUserRequest.newBuilder().apply { setUserId(userId) }.build()
-
-        val expectedResponse = DeleteUserResponse.newBuilder().apply {
-            successBuilder.build()
-        }.build()
-        val actual = doRequest(NatsSubject.DELETE_USER_BY_ID, request, DeleteUserResponse.parser())
-
-        assertThat(actual).isEqualTo(expectedResponse)
-    }
-
-    @Test
-    fun `Should return success response for get user by ID`() {
-
-        val request = GetByIdUserRequest.newBuilder().setUserId(USER_ID).build()
-
-        val expectedResponse = GetByIdUserResponse.newBuilder().apply {
-            successBuilder.setUser(protoUser)
-        }.build()
-        val actual = doRequest(NatsSubject.GET_USER_BY_ID, request, GetByIdUserResponse.parser())
-
-        assertThat(actual).isEqualTo(expectedResponse)
-    }
     private fun <RequestT : GeneratedMessageV3, ResponseT : GeneratedMessageV3> doRequest(
         subject: String,
         payload: RequestT,
