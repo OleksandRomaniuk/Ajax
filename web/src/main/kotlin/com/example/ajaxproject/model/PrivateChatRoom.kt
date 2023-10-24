fd146ad24853ebc9eddcbe23df0079de3550b9e2
oleksandrromanuk <alehandr.romaniuk@gmail.com>
2023-10-24 17:46:41 +0300

    Fix pr

--- web/src/main/kotlin/com/example/ajaxproject/model/PrivateChatRoom.kt
+++ web/src/main/kotlin/com/example/ajaxproject/model/PrivateChatRoom.kt
@@ -1,11 +1,10 @@
 package com.example.ajaxproject.model
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("private-chat-room")
-data class PrivateChatRoom @JsonCreator constructor(
+data class PrivateChatRoom(
     @JsonProperty("id")
     val id: String,
     @JsonProperty("senderId")
